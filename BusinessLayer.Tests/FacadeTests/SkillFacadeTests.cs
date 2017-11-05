bea975efe6f28dc3c92b741562e21bfa926ab5bb
Ondrej Oravcok <pista.baci@atlas.sk>
2017-11-05 19:46:43 +0100

    SkillTag facade

---
+++ BusinessLayer.Tests/FacadeTests/SkillFacadeTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using BusinessLayer.DataTransferObjects;
+using BusinessLayer.DataTransferObjects.Filters;
+using BusinessLayer.Facades;
+using BusinessLayer.QueryObjects.Common;
+using BusinessLayer.Services.Skills;
+using BusinessLayer.Tests.FacadeTests.Common;
+using DAL.Entities;
+using Infrastructure.Query;
+using Infrastructure.Repository;
+using Moq;
+using NUnit.Framework;
+
+namespace BusinessLayer.Tests.FacadeTests
+{
+    class SkillFacadeTests
+    {
+        [Test]
+        public async Task CreateSkill()
+        {
+            var mockManager = new FacadeMockManager();
+            var skillRepositoryMock = mockManager.ConfigureCreateRepositoryMock<SkillTag>(nameof(SkillTag.Id));
+            var skillQueryMock =
+                mockManager.ConfigureQueryObjectMock<SkillTagDto, SkillTag, SkillTagFilterDto>(null);
+            var skillFacade = CreateFacade(skillQueryMock, skillRepositoryMock);
+
+            await skillFacade.CreateSkill(new SkillTagDto
+                {
+                    Name = "C# master"
+                }
+            );
+
+            Assert.AreNotEqual(0, mockManager.CapturedCreatedId);
+        }
+
+
+        private static SkillFacade CreateFacade(
+            Mock<QueryObjectBase<SkillTagDto, SkillTag, SkillTagFilterDto, IQuery<SkillTag>>> skillQueryMock,
+            Mock<IRepository<SkillTag>> skillRepositoryMock)
+        {
+            var mockManager = new FacadeMockManager();
+            var uowMock = FacadeMockManager.ConfigureUowMock();
+            var mapper = FacadeMockManager.ConfigureRealMapper();
+            var service = new SkillService(mapper, skillRepositoryMock.Object, skillQueryMock.Object);
+            var facade = new SkillFacade(uowMock.Object, service);
+            return facade;
+        }
+    }
+}
---
+++ BusinessLayer/Facades/SkillFacade.cs
@@ -0,0 +1,65 @@
+﻿using System.Threading.Tasks;
+using BusinessLayer.DataTransferObjects;
+using BusinessLayer.DataTransferObjects.Common;
+using BusinessLayer.DataTransferObjects.Filters;
+using BusinessLayer.Facades.Common;
+using BusinessLayer.Services.Skills;
+using Infrastructure.UnitOfWork;
+
+namespace BusinessLayer.Facades
+{
+    public class SkillFacade : FacadeBase
+    {
+        private readonly ISkillService skillService;
+
+        public SkillFacade(IUnitOfWorkProvider unitOfWorkProvider, ISkillService skillService) : base(unitOfWorkProvider)
+        {
+            this.skillService = skillService;
+        }
+
+        public async Task CreateSkill(SkillTagDto skill)
+        {
+            using (var unitOfWork = UnitOfWorkProvider.Create())
+            {
+                skillService.Create(skill);
+                await unitOfWork.Commit();
+            }
+        }
+
+        public async Task<bool> EditSkill(SkillTagDto skill)
+        {
+            using (var uow = UnitOfWorkProvider.Create())
+            {
+                if ((await skillService.GetAsync(skill.Id, false)) == null)
+                {
+                    return false;
+                }
+                await skillService.Update(skill);
+                await uow.Commit();
+                return true;
+            }
+        }
+
+        public async Task<bool> DeleteSkill(int skillId)
+        {
+            using (var uow = UnitOfWorkProvider.Create())
+            {
+                if ((await skillService.GetAsync(skillId, false)) == null)
+                {
+                    return false;
+                }
+                skillService.Delete(skillId);
+                await uow.Commit();
+                return true;
+            }
+        }
+
+        public async Task<QueryResultDto<SkillTagDto, SkillTagFilterDto>> GetAllSkillsAsync()
+        {
+            using (UnitOfWorkProvider.Create())
+            {
+                return await skillService.ListAllAsync();
+            }
+        }
+    }
+}
